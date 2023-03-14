6bae6795e7fbd96ae28274dbe80d82a7e94897c3
Belovinceva <Belovinceva@.lv>
2023-03-14 10:12:51 +0200

    Informacijas vaicajums par klientu

--- src/Belovinceva_pica.java
+++ src/Belovinceva_pica.java
@@ -3,9 +3,21 @@ import javax.swing.JOptionPane;
 public class Belovinceva_pica {
 
 	public static void main(String[] args) {
-		int choice = JOptionPane.showOptionDialog(null, "Atlasît darbîbu:", "Darbîba",
+		int darbibas = JOptionPane.showOptionDialog(null, "Atlasît darbîbu:", "Darbîba",
 				JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null,
 				new String[]{"Pievienot jaunu pasûtîjumu", "Pasûtîjumu saraksts"}, "Pievienot jaunu pasûtîjumu");
+		switch(darbibas){
+		case 0:
+			String name = JOptionPane.showInputDialog("Ievadît klienta nosaukumu:");
+			String phone = JOptionPane.showInputDialog("Ievadiet klienta tâlruòa numuru:");
+			String email = JOptionPane.showInputDialog("Ievadît klienta e-pastu:");
+			break;
+			
+		case 1:
+			
+			break;
+			
+		}
 	}
 
 }
